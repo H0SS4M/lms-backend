a89253001cdaf9ede720fb39762f97acfce01b6b
Hossam-Eldin Mohammed <82487239+H0SS4M@users.noreply.github.com>
2025-08-17 15:20:45 +0300

    adding CustomUserDetails class

---
+++ src/main/java/com/lms/lms_backend/security/CustomUserDetails.java
@@ -0,0 +1,66 @@
+package com.lms.lms_backend.security;
+
+import com.lms.lms_backend.model.User;
+import com.lms.lms_backend.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+    
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.getActive();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.getActive();
+    }
+
+    public Long getId() {
+        return user.getId();
+    }
+
+    public UserRole getRole() {
+        return user.getRole();
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
